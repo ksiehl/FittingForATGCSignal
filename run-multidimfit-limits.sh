b1bff017dad2a1d118ff6edeabfa4f41bfb84ca3
Kevin Siehl <kevin.siehl@wayne.edu>
2019-10-17 23:06:35 +0200

    updating condor stuff

--- run-multidimfit-limits.sh
+++ run-multidimfit-limits.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
 pwd
+source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc6_amd64_gcc481
 eval `scramv1 project CMSSW CMSSW_7_1_5`
+#cp libHiggsAnalysisCombinedLimit.so CMSSW_7_1_5/lib/slc6_amd64_gcc481/
 cd CMSSW_7_1_5/src/
 eval `scramv1 runtime -sh`
 echo "CMSSW: "$CMSSW_BASE
 cd -
 
+echo "we've gotten this far. Now let's quit." 
 #1-D Limits
 #----------
 
-combine workspace_simfit.root -M MultiDimFit --floatOtherPOIs=0 --algo=grid --expectSignal=1 --points=100 --redefineSignalPOIs cwww -P cwww --freezeNuisances ccw,cb --setPhysicsModelParameters cwww=0,ccw=0,cb=0 --setPhysicsModelParameterRange cwww=-3.6,3.6 --minimizerStrategy=2 --cminPreScan -n _cwww_3.6
+#./combine workspace_simfit.root -M MultiDimFit --floatOtherPOIs=0 --algo=grid --expectSignal=1 --points=2 --redefineSignalPOIs cwww -P cwww --freezeNuisances ccw,cb --setPhysicsModelParameters cwww=0,ccw=0,cb=0 --setPhysicsModelParameterRange cwww=-3.6,3.6 --minimizerStrategy=2 --cminPreScan -n _cwww_3.6
 
 #combine workspace_simfit.root -M MultiDimFit --floatOtherPOIs=0 --algo=grid --expectSignal=1 --points=1000 --redefineSignalPOIs ccw -P ccw --freezeNuisances cwww,cb --setPhysicsModelParameters cwww=0,ccw=0,cb=0 --setPhysicsModelParameterRange ccw=-4.5,4.5 --minimizerStrategy=2 --cminPreScan -n _ccw_4.5
 
